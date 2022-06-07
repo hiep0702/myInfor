c02d7d54d820969efc0f7a8794621cd3d2d80663
Tran-Tuan-Hiep <trantuanhiep07022001@gmail.com>
2022-06-07 17:50:32 +0700

    push file myinfor

---
+++ myInfor.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Thông tin cá nhân</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body> 
+	<style type="text/css">
+		.item{
+			margin-left:10% ;
+		}
+		img{
+			margin-left: 12%;
+		}
+		body{
+			margin-top: 35px;
+		}
+		.test{
+			float: right;
+		}
+	</style>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3" style="background-color: #E6E6FA">
+				<img src="0702.jpg" width="200px" height="200px" alt="" style="border-radius:50%">
+				<div class="item" style="margin-top: 50px; height: 124px;" >
+					<p>Giới tính: Nam</p>
+					<p>Năm sinh: 2001</p>
+					<p>Di động: 0363251199</p>
+					<p>Email: tuanhiep070201@mail.com</p>
+				</div>
+			</div>
+			<div class="col-md-9" style="background-color: #D8BFD8;">
+				<div style="float: right; display:block; width:100%">
+					<div class="test">
+						<h1 style="font-size: 40px">Trần Tuấn Hiệp</h1>
+					<h4 style="margin-left: 50px">Lập trình web (Backend)</h4>
+					</div>
+				</div>
+				
+				<div style="padding-top:100px">
+					<hr>
+					<h3>Học vấn</h3>
+					<p>	- Trường đại học công nghệ giao thônng vận tải</p>
+					<p>	- Khoa công nghệ thông tin</p>
+					<p> - Điểm trung bình: 2.8/4.0</p>
+					<h3>Sở thích</h3>
+					<p> - Đá bóng</p>
+					<p> - Tìm hiểu đồ công nghệ</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
